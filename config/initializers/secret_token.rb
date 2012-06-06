a7164f7b402ce8d8f7065b83df7f66aa25c8f310
Salym Senyonga <salymsash@gmail.com>
2012-06-06 20:59:27 +0300

    added pg configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '47e7ec3eb9bf7dcb4b0171fae7d1622dbb427dadfc5252657b8251aed911a425381b6f546f91bac36b6e84eac1e319973151cc44f1f02e5bc455b0efcbdafa0a'
