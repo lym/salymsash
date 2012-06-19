97e82ffca6a78b0751839e5d86e68459b6e2055d
Salym Senyonga <salymsash@gmail.com>
2012-06-19 15:58:43 +0300

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ActiveRecord::Base
+    attr_accessible :body, :excerpt, :location, :published_at, :title
+	validates :title, :presence => true
+	validates :body, :presence => true
+
+	def long_title
+		"#{title} - #{published_at}"
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+    attr_accessible :email, :password
+	
+	has_one :profile
+end
