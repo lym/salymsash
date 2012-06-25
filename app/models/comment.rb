662adde0eac2b4311513bf8d6fc4a3763d1560f2
Salym Senyonga <salymsash@gmail.com>
2012-06-26 00:02:34 +0300

    added a few callbacks to comments

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :article_id, :body, :email, :name
+  belongs_to :article
+
+  validates :name, :email, :body, :presence => true
+  validate :article_should_be_published
+
+  after_create :email_article_author
+
+  def article_should_be_published
+    errors.add(:article_id, "is not published yet") if article && !article.published?
+  end
+
+  def email_article_author
+	puts "we will notify the author in chapter 9" 
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
     attr_accessible :email, :password
+
+	validates :email, :uniqueness => true, 
+					  :length => { :within => 5..50 },
+					  :format => { :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i }
+	validates :password, :confirmation => true
 	
 	has_one :profile
+	has_many :articles, :order => 'published_at DESC, title ASC',
+						:dependent => :nullify 
+	has_many :replies, :through => :articles, :source => :comments
 end
