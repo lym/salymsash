b59ec92d8c61e3a5ab7278726e6cb8bf73eb83b3
Salym Senyonga <salymsash@gmail.com>
2012-08-16 22:48:07 +0300

    added gmail smtp server settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Blog::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :domain         => "domain.of.sender.net",
+    :authentication => "plain",
+    :user_name      => "dave",
+    :password       => "secret",
+    :enable_starttls_auto => true
+  }
   # Code is not reloaded between requests
   config.cache_classes = true
 
